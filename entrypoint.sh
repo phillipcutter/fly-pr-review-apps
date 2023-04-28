66a888414b45bcc83a9c1f2a4b400ce6ab1e1e3f
Phillip Cutter <mrfleap@gmail.com>
2023-04-28 14:48:11 -0700

    Add --auto-confirm to fly commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,13 +37,13 @@ fi
 
 # Deploy the Fly app, creating it first if needed.
 if ! flyctl status --app "$app"; then
-  flyctl launch --no-deploy --copy-config --name "$app" --region "$region" --org "$org"
+  flyctl launch --no-deploy --copy-config --name "$app" --region "$region" --org "$org" --auto-confirm
   if [ -n "$INPUT_SECRETS" ]; then
     echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app"
   fi
-  flyctl deploy --app "$app" --region "$region" --region "$region" --strategy immediate
+  flyctl deploy --app "$app" --region "$region" --region "$region" --strategy immediate --auto-confirm
 elif [ "$INPUT_UPDATE" != "false" ]; then
-  flyctl deploy --config "$config" --app "$app" --region "$region" --region "$region" --strategy immediate
+  flyctl deploy --config "$config" --app "$app" --region "$region" --region "$region" --strategy immediate --auto-confirm
 fi
 
 # Attach postgres cluster to the app if specified.
